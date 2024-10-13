6b23315bb7d9ddb77c48d6930c45b0ce12d80a6b
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-13 11:18:04 +0100

    string concatination function while minding the Type anotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Write a type-annotated function concat that
+takes a string
+
+str1 and a string str2 as arguments and returns a
+concatenated string
+"""
+
+
+def concat(str1: str, str2: str):
+    """Combines two strings together/
+    Concatinates two strings
+    """
+
+    return str1 + str2
