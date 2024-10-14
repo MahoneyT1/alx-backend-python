e54b1937bdaf4a9a243f27022236097c12518667
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-14 16:08:01 +0100

    running two coroutine at the same time,

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-
-"""
-Import wait_random from the previous python file
+""" Import wait_random from the previous python file
 that you’ve written and write an async routine called
 wait_n that takes in 2 int arguments (in this order): n
 and max_delay. You will spawn wait_random n times with
@@ -11,6 +9,7 @@ wait_n should return the list of all the delays (float values).
 The list of the delays should be in ascending order without using
 sort() because of concurrency.
 """
+
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
