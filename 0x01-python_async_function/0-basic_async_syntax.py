5995ae12535f336b2aeb3b643e0cea4623e04eec
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-14 15:37:00 +0100

    func that returns a delayed floating number

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,23 +1,20 @@
-#!/usr/bin/env python3
-"""Write an asynchronous coroutine that takes in an integer
-argument (max_delay, with a default value of 10) named
-wait_random that waits for a random delay between 0 and max_delay
-(included and float value) seconds and eventually returns it.
+# #!/usr/bin/env python3
+
+""" Module that contains an asynchronous coroutine that takes an integer
+argument (max_delay, with a default value of 10) named wait_random
+that waits for a random delay between 0 and max_delay (included float value)
+seconds and eventually returns it.
 """
 
 import asyncio
 import random
 
 
-async def wait_random(max_delay: int=10) -> float:
-    """Function that returns a float delay number
-    Args:
-        (int)
-
-    Returns:
-        A random floating number
+async def wait_random(max_delay: int = 10) -> float:
+    """ Asynchronous coroutine that takes an integer argument
+    and returns it after a delay.
     """
-
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
+
     return delay
