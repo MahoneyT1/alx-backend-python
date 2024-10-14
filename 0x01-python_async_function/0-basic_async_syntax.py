816be59dc68c2998bd7d7868accd51909036f742
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-14 15:35:06 +0100

    func that returns a delayed floating number

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -21,7 +21,3 @@ async def wait_random(max_delay: int=10) -> float:
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
-
-
-if __name__ == "__main__":
-    asyncio.run(wait_random())
