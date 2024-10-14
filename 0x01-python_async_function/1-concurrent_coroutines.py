69d70cb70f585024271dcdf1a3c94ce2417499b0
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-14 16:06:59 +0100

    running two coroutine at the same time,

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -16,9 +16,15 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> float:
+    """wait_n should return the list of all the delays
+    (float values). The list of the delays should be in
+    ascending order without using sort() because of
+    concurrency.
+    """
+
     result = []
 
-    for i in range(n):
+    for _ in range(n):
         result.append(await wait_random(max_delay))
 
     result = sorted(result)
