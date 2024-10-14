a1ea8d607859c6baca3c762cf3f9d1bc7e3f3c3d
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-14 17:20:45 +0100

    returned a class asyncio class

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -12,6 +12,7 @@ import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """Write a function (do not create an async function,
     use the regular function syntax to do this) task_wait_random
