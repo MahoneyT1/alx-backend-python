f5a725b67924d51c67f379a7c2098288f63e3185
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-16 14:32:53 +0100

    using async co-routine to yield random numbers

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,7 +12,7 @@ from typing import Generator, List
 
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> Generator[float, None]:
     """
     then yield a random number between 0 and 10. Use the random module
     """
