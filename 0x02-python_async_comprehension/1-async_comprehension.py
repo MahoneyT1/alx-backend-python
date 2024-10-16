059f660edc1d80971d4c95132fe0fc55187f215c
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-16 15:04:54 +0100

    corotine that that collects a generator

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,10 +6,10 @@ that takes no arguments. The coroutine will collect
 10 random numbers using an async comprehensing over
 async_generator, then return the 10 random numbers.
 """
+
 import asyncio
 import random
 from typing import Generator
-
 async_generator = __import__('0-async_generator').async_generator
 
 
