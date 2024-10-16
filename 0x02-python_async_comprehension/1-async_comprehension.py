f85af96fc869738a1470745c698754c595717d41
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-16 15:03:23 +0100

    corotine that that collects a generator

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -13,7 +13,7 @@ from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> Generator[float, float, None]:
     """A co-routine function that returns a generated number"""
     random_num_gen = [ran_num async for ran_num in async_generator()]
     return random_num_gen
