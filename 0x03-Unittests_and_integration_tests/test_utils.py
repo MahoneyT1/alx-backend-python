da96d541b0d9d7f41b5143ccf225bc01651822fc
MahoneyT1 <ifeanyitech15@gmail.com>
2024-11-02 14:15:38 +0100

    using parameterized to parse args to method

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Familiarize yourself with the utils.access_nested_map function
+and understand its purpose. Play with it in the Python console
+to make sure you understand.
+
+In this task you will write the first unit test for utils.access_nested_map.
+
+Create a TestAccessNestedMap class that inherits from unittest.TestCase.
+
+Implement the TestAccessNestedMap.test_access_nested_map method to
+test that the method returns what it is supposed to.
+
+Decorate the method with @parameterized.expand to test the function
+for following inputs:
+
+nested_map={"a": 1}, path=("a",)
+nested_map={"a": {"b": 2}}, path=("a",)
+nested_map={"a": {"b": 2}}, path=("a", "b")
+For each of these inputs, test with assertEqual that the function
+returns the expected result.
+
+The body of the test method should not be longer than 2 lines.
+"""
+from unittest.mock import Mock
+import unittest
+from parameterized import parameterized
+
+access_nested_map = __import__("utils").access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test the access class"""
+
+    @parameterized.expand([
+        ({"nested_map": {"a": 1}, "path": ("a",)}, 1),
+        ({"nested_map": {"a": {"b": 2}}, "path": ("a",)}, {'b': 2}),
+        ({"nested_map": {"a": {"b": 2}}, "path": ("a", "b")}, 2),
+    ])
+    def test_access_nested_map(self, input, expected):
+        """check if it matches the expected answer"""
+        self.assertEqual(access_nested_map(input['nested_map'],
+                                           input['path']), expected)
